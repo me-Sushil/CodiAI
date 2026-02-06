b75a5dde448559092354f14224b3e3460a06b9f7
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2026-02-06 12:09:40 +0545

    changededwekjfhdsjkn

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -1,5 +1,5 @@
   const LoadingAnimation = () => (
-    <div className="flex justify-start">
+    <div className="flex justify-start">fdfdgfdg
       <div className="bg-gradient-to-r from-cyan-500/10 via-violet-500/10 to-fuchsia-500/10 backdrop-blur-sm border border-violet-400/30 p-3 rounded-xl max-w-3xl relative overflow-hidden">
         <div className="absolute inset-0 rounded-xl bg-gradient-to-r from-cyan-400/5 via-violet-400/5 to-fuchsia-400/5"></div>
         <div className="flex items-center space-x-2 relative z-10">
